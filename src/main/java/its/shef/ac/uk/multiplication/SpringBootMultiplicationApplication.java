0e1ad1894358430f8139467bcfc299f75c9565a8
Jingyu Qi <cs1jqi@sheffield.ac.uk>
2023-01-24 23:16:46 +0000

    init

---
+++ src/main/java/its/shef/ac/uk/multiplication/SpringBootMultiplicationApplication.java
@@ -0,0 +1,13 @@
+package its.shef.ac.uk.multiplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMultiplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMultiplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/its/shef/ac/uk/multiplication/SpringBootMultiplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package its.shef.ac.uk.multiplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMultiplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
